c39c75cdcb6f20ec6bbade314b5c8cb5992cc696
MikhaelMounay <mikhaelmounay@gmail.com>
2024-12-17 00:06:20 +0200

    (fix): change the diff command to FC only on win32/win64 for undotree plugin

--- lua/mikhaelmounay/lazy-plugins/undotree.lua
+++ lua/mikhaelmounay/lazy-plugins/undotree.lua
@@ -2,7 +2,9 @@ return {
     "mbbill/undotree",
 
     config = function() 
-	vim.g.undotree_DiffCommand = "FC"
+        if vim.fn.has("win32") == 1 or vim.fn.has("win64") == 1 then
+            vim.g.undotree_DiffCommand = "FC"
+        end
 
         vim.keymap.set("n", "<leader>u", vim.cmd.UndotreeToggle)
     end
